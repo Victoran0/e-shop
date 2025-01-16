5f022b378376cf7c80f65b8944c184f1b3018dae
Victoran0 <victoranosunkvic@gmail.com>
2025-01-16 04:08:24 +0100

    Displya List of items and Header component on the Home page

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,13 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { PRODUCTS } from '../../../assets/products'
+import { ProductListItem } from '../../components/product-list-item'
+import { ListHeader } from '../../components/list-header'
 
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <FlatList 
+        data={PRODUCTS}
+        renderItem={({item}) => (
+          <ProductListItem product={item} />
+        ) }
+        keyExtractor={item => item.id.toString()}
+        numColumns={2}
+        ListHeaderComponent={ListHeader}
+        contentContainerStyle={styles.FlatListContent}
+        columnWrapperStyle={styles.flatListColumn}
+        style={styles.listStyle}
+      />
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  FlatListContent: {
+    paddingBottom: 20,
+  },
+  flatListColumn: {
+    justifyContent: 'space-between',
+  },
+  listStyle: {
+    paddingHorizontal: 10,
+    paddingVertical: 5
+  }
+
+})
